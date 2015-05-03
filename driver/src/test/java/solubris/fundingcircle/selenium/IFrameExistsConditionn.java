0a0a873c52b5289607ed77be38d8b6b190005f73
lithium147 <timlwalters@yahoo.co.uk>
2015-05-03 02:08:53 +0100

    remove some of the sleeps

---
+++ driver/src/test/java/solubris/fundingcircle/selenium/IFrameExistsConditionn.java
@@ -0,0 +1,32 @@
+package solubris.fundingcircle.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class IFrameExistsConditionn implements ExpectedCondition<WebElement> {
+    private static final Logger logger = LoggerFactory.getLogger(AjaxConnectionsCondition.class);
+
+    private final String id;
+
+    public IFrameExistsConditionn(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public WebElement apply(WebDriver driver) {
+        final List<WebElement> iframes = driver.findElements(By.tagName("iframe"));
+        for (WebElement iframe : iframes) {
+            if (iframe.getAttribute("id").equals(id)) {
+                return iframe;
+            }
+        }
+
+        return null;
+    }
+}
